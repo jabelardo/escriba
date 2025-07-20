19ab4a7ee8bd6483ae6c3097e349c581f5c9d866
Jose Gutierrez <jabelardo@gmail.com>
2025-07-20 11:44:44 +0200

    toast

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Flex } from "@radix-ui/themes";
+import { Flex, Portal } from "@radix-ui/themes";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { EditorPanel } from "./Editor/EditorPanel";
 import ErrorBoundary from "./ErrorBoundary";
@@ -10,14 +10,18 @@ import { NotificationProvider } from "./ui/Notification";
 
 export const Layout = () => {
   return (
-    <Flex height="100vh" overflow="hidden" direction="row">
-      <ErrorBoundary>
-        <Sidebar />
-        <RealmProvider>
-          <EditorPanel />
-        </RealmProvider>
+    <>
+      <Flex height="100vh" overflow="hidden" direction="row">
+        <ErrorBoundary>
+          <Sidebar />
+          <RealmProvider>
+            <EditorPanel />
+          </RealmProvider>
+        </ErrorBoundary>
+      </Flex>
+      <Portal>
         <NotificationProvider />
-      </ErrorBoundary>
-    </Flex>
+      </Portal>
+    </>
   );
 };
--- src/components/ui/Notification.tsx
+++ src/components/ui/Notification.tsx
@@ -4,12 +4,14 @@ import * as Toast from "@radix-ui/react-toast";
 import { useNotificationStore } from "@/store/notificationStore";
 import { CheckCircledIcon, CrossCircledIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 
-const typeClasses = {
-    success: 'bg-green-500',
-    error: 'bg-red-500',
-    warning: 'bg-yellow-500',
-    info: 'bg-blue-500',
-  };
+import "./Notification.css";
+
+const typeIcons = {
+  success: <CheckCircledIcon className="ToastIcon" />,
+  error: <CrossCircledIcon className="ToastIcon" />,
+  warning: <InfoCircledIcon className="ToastIcon" />,
+  info: <InfoCircledIcon className="ToastIcon" />,
+};
 
 export const NotificationProvider = () => {
   const { notifications, removeNotification } = useNotificationStore();
@@ -19,23 +21,19 @@ export const NotificationProvider = () => {
       {notifications.map(({ id, type, title, message }) => (
         <Toast.Root
           key={id}
-          className={`list-none rounded-md p-4 text-white shadow-lg ${typeClasses[type]}`}
+          className="ToastRoot"
+          data-type={type}
           onOpenChange={() => removeNotification(id)}
         >
-            <div className="flex items-center gap-4">
-                {type === 'success' && <CheckCircledIcon className="w-6 h-6" />}
-                {type === 'error' && <CrossCircledIcon className="w-6 h-6" />}
-                {type === 'warning' && <InfoCircledIcon className="w-6 h-6" />}
-                {type === 'info' && <InfoCircledIcon className="w-6 h-6" />}
-                <div>
-                    <Toast.Title className="font-bold">{title}</Toast.Title>
-                    <Toast.Description>{message}</Toast.Description>
-                </div>
-            </div>
+          {typeIcons[type]}
+          <Toast.Title className="ToastTitle">{title}</Toast.Title>
+          <Toast.Description className="ToastDescription">
+            {message}
+          </Toast.Description>
           <Toast.Close />
         </Toast.Root>
       ))}
-      <Toast.Viewport className="fixed bottom-0 right-0 z-50 flex w-96 flex-col gap-2 p-4" />
+      <Toast.Viewport className="ToastViewport" />
     </Toast.Provider>
   );
 };
