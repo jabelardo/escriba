ec8e15b866aa146bc9ac4e6790c8f5945a5a9ba5
Jose Gutierrez <jabelardo@gmail.com>
2026-01-27 22:14:13 +0100

    deleted:    src/components/ErrorBoundary.tsx 	modified:   src/components/Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,6 @@
 import { Flex, Portal } from "@radix-ui/themes";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { EditorPanel } from "./Editor/EditorPanel";
-import ErrorBoundary from "./ErrorBoundary";
 import { RealmProvider } from "@mdxeditor/editor";
 
 import { NotificationProvider } from "./ui/Notification";
@@ -11,7 +10,6 @@ import { NotificationProvider } from "./ui/Notification";
 export const Layout = () => {
   return (
     <>
-      <ErrorBoundary>
         <Flex height="100vh" overflow="hidden" direction="row">
           <Sidebar />
           <RealmProvider>
@@ -21,7 +19,6 @@ export const Layout = () => {
         <Portal>
           <NotificationProvider />
         </Portal>
-      </ErrorBoundary>
     </>
   );
 };
